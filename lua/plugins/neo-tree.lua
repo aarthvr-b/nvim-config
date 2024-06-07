9ad0dad2581fb57fceba8bbe2ff9fe658ac0e275
aarthvr-b <assefarthur@gmail.com>
2024-06-08 01:41:07 +0200

    Neotree config changes
    Configured neotree to show hidden items.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,18 @@ return{
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       },
-  config = function()
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      }
+    }
+  },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
   end
 }
