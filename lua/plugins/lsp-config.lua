ef625d6b325f119213fb9d1e6bd2069129f395a1
Arthur J. Barbosa <103223206+aarthvr-b@users.noreply.github.com>
2024-11-12 13:06:58 +0100

    added more lsp to treesitter

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,8 +9,30 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "terraformls", "tflint" },
-			})
+				ensure_installed = {
+						"json",
+					        "javascript",
+					        "typescript",
+					        "tsx",
+					        "yaml",
+					        "html",
+					        "css",
+					        "markdown",
+					        "markdown_inline",
+					        "graphql",
+					        "bash",
+					        "lua",
+					        "vim",
+					        "dockerfile",
+					        "gitignore",
+					        "query",
+					        "vimdoc",
+						"lua_ls",
+						"pyright",
+						"terraformls",
+						"tflint" },
+			}
+		)
 		end,
 	},
 	{
