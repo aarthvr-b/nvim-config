6d3d9cdc0e7ddfd3b66d829b55e5e0fe86f2bb41
Arthur J. Barbosa <103223206+aarthvr-b@users.noreply.github.com>
2024-11-12 12:58:10 +0100

    Updated lualine.lua settings

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,25 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-   require('lualine').setup({
-    option = {
-      theme = "dracula"
-    }
-  })
-  end
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+
+    -- configure lualine with modified theme
+    lualine.setup({
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+    -- color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+    })
+  end,
 }
