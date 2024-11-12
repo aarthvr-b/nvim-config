191799278e2eb9db149f08db37b7ae97a49ab332
Arthur J. Barbosa <103223206+aarthvr-b@users.noreply.github.com>
2024-11-12 12:53:42 +0100

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,21 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = {  
+        'nvim-lua/plenary.nvim',
+        "nvim-tree/nvim-web-devicons",
+        "folke/todo-comments.nvim"
+     },
+
+config = function()
+      require('telescope').setup({})
+      
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n','<leader>fg', builtin.live_grep,{})
-      vim.keymap.set('n', '<C-p>', builtin.find_files,{})
-    end
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = "Telescope find files"})
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = "Telescope find Git Files"})
+      vim.keymap.set('n', '<leader>ps', builtin.live_grep, {desc = "Telescope Live grep"})
+      vim.keymap.set('n', '<leader>pt', "<cmd>TodoTelescope<cr>", {desc = "Find todos"})
+  end
   },
   {'nvim-telescope/telescope-ui-select.nvim',
    config = function ()
