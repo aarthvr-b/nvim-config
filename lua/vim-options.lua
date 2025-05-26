54270b66e430894aac7958ba76f2e91baa7ea3f6
abarbosa <arthur.barbosa@zenithglobal.eu>
2025-05-26 12:32:27 +0200

    Added new keymap to close file, <leader>bd

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,26 +14,11 @@ vim.opt.listchars = {
 	trail = "•",
 	nbsp = "␣",
 }
-
+vim.opt.fileformats = { "unix" }
 
 vim.g.mapleader = " "
-
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex , { desc = "Open file explorer"})
-
--- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"})
-vim.keymap.set("n", "<leader>sh", "<C-w>s", {desc = "Split window horizontally"})
-vim.keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make split equal size"})
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current split"})
-
--- tab management
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "close tab"})
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "go to next tab"})
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "go to prev tab"})
-
-
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", {desc = "Delete current buffer"})
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {desc = "Format current Buf"})
 
